36ede8616a9788477a8318665bfb1e0ca3d8f3ad
sahajSinghal <sahajsinghal2007@gmail.com>
2020-10-01 17:00:17 +0530

    Added Start Image

--- js/sketch.js
+++ js/sketch.js
@@ -13,6 +13,15 @@ var ground;
 //variables for platforms
 var platform1, platform2, platform3, platform4, platform5;
 
+var gameState = "startPage";
+
+var startImage;
+
+function preload()
+{
+    startImage = loadImage("Images/CoverPage.png");
+}
+
 function setup()
 {
     //creating the canvas
@@ -24,16 +33,23 @@ function setup()
 
     //creating the player body
     player = new Player(100,765);
+    player.playerSprite.visible = false;
 
     //creating the ground body
     ground = new Ground(600,790,1200,20);
+    ground.groundSprite.visible = false;
 
     //creating the platforms
     platform1 = new Platform(320,700,200,20);
+    platform1.platformSprite.visible = false;
     platform2 = new Platform(540,500,200,20);
+    platform2.platformSprite.visible = false;
     platform3 = new Platform(740,600,200,20);
+    platform3.platformSprite.visible = false;
     platform4 = new Platform(990,470,200,20);
+    platform4.platformSprite.visible = false;
     platform5 = new Platform(840,260,200,20);
+    platform5.platformSprite.visible = false;
         
     //running the engine
     Engine.run(engine);
@@ -44,18 +60,23 @@ function draw()
     //background is white
     background("white");
 
-    //displaying and moving the player
-    player.display();
+    if(gameState === "startPage")
+    {
+        background(startImage);
+    }
 
-    //displaying the ground
-    ground.display();
+    // //displaying and moving the player
+    // player.display();
 
-    //displaying the platforms
-    platform1.display();
-    platform2.display();
-    platform3.display();
-    platform4.display();
-    platform5.display();
+    // //displaying the ground
+    // ground.display();
+
+    // //displaying the platforms
+    // platform1.display();
+    // platform2.display();
+    // platform3.display();
+    // platform4.display();
+    // platform5.display();
 
     //making sprites visible
     drawSprites();
